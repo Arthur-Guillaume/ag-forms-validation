a5fd2e68a8400996419cbfc13c5932d68373eaf9
Arthur Guillaume <arthur.guillaume@buzznative.com>
2017-09-13 17:56:03 +0200

    Changed attribute fetch

--- agformsvalidation/src/main/java/com/arthurguillaume/android/agformsvalidation/PasswordConfirmationInput.java
+++ agformsvalidation/src/main/java/com/arthurguillaume/android/agformsvalidation/PasswordConfirmationInput.java
@@ -28,7 +28,7 @@ public class PasswordConfirmationInput extends AppCompatEditText implements IVal
         super(context, attrs);
         TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.PasswordConfirmationInput);
         referencedField = attributes.getResourceId(R.styleable.PasswordConfirmationInput_confirm_password, NO_ID);
-        String userDefinedErrorMessage = attributes.getString(R.styleable.ValidatedTextInput_errorMessage);
+        String userDefinedErrorMessage = attributes.getString(R.styleable.PasswordConfirmationInput_errorMessage);
         if (userDefinedErrorMessage != null) {
             errorMessage = userDefinedErrorMessage;
         } else {
